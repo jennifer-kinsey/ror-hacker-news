55b71d43e2382fbaebce9e3d0d7cdbc124a39929
Jennifer Kinsey and Spencer Alan <jennifer@>
2017-06-27 11:18:05 -0700

    upvote with custom route

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,27 @@
 class LinksController < ApplicationController
 
   def index
-    @links = Link.all
+    @links = sorted_links
   end
 
+  def up_vote
+    link = Link.find(params[:id])
+    link.rank += 1
+    link.save
+    redirect_to links_path
+  end
+
+  def down_vote
+    link = Link.find(params[:id])
+    link.rank -= 1
+    link.save
+    redirect_to links_path
+  end
+
+private
+
+  def sorted_links
+    links = Link.all
+    links.sort { |x,y| y.rank <=> x.rank }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :links do
     resources :comments
   end
+
+  post 'up_vote/:id' => 'links#up_vote', as: "up_vote"
+  post 'down_vote/:id' => 'links#down_vote', as: "down_vote"
 end
